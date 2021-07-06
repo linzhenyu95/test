d8708f531f14d3de0da0db07423793970dd2898f
大梦初醒 <65234274+linzhenyu95@users.noreply.github.com>
2021-07-07 01:06:01 +0800

    Merge branch 'master' of github.com:linzhenyu95/test
    # Conflicts:
    #	test01.py

--- demo.py
+++ demo.py
@@ -3,3 +3,24 @@
 # @Author   :Lins
 # @Time     :2021/7/7 1:01
 # @File     :demo.py
+import pytest
+import pytest
+# 作用域：module是在是在模块之前执行，模块之后执行
+@pytest.fixture(scope="module")
+def open():
+    print("打开浏览器")
+    yield
+
+    print("执行teardown!")
+    print("最后关闭浏览器")
+
+def test_search1(open):
+    print("test_search1")
+    raise NameError
+    pass
+
+def test_search2(open):
+    print("test_search2")
+    pass
+if __name__== '__main__':
+    pytest.main()
